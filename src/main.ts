3695ebc531e3ea708819ce14be05c12fda7361fe
Kristian Ekenes <kekenes@esri.com>
2024-01-27 15:40:54 -0800

    move legend to panel

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,56 @@ defineCalciteElements(window, {
 
 import {
   FeaturesWidget,
+  LegendWidget,
   SearchWidget,
   defineCustomElements as defineMapElements,
 } from "@arcgis/map-components/dist/loader";
 defineMapElements();
 
 import { on } from "@arcgis/core/core/reactiveUtils";
+import uniqueValues from "@arcgis/core/smartMapping/statistics/uniqueValues";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
 const mapElement = document.querySelector<HTMLArcgisMapElement>("arcgis-map");
 
 const loadMap = async () => {
   const view = mapElement?.view;
+  const map = view?.map;
+
+  const countiesLayer = map?.layers.find((layer) => layer.title === "Counties (fylke)") as __esri.FeatureLayer;
+  const parishLayer = map?.layers.find((layer) => layer.title === "Parishes (sokn)") as __esri.FeatureLayer;
 
   const featuresElement =
     document.querySelector<HTMLArcgisFeaturesElement>("arcgis-features");
   const searchElement =
     document.querySelector<HTMLArcgisSearchElement>("arcgis-search");
+  const legendElement =
+    document.querySelector<HTMLArcgisLegendElement>("arcgis-legend");
+
+  const countyComboBoxElement = document.getElementById("county-combobox") as HTMLCalciteComboboxElement;
+  const parishComboBoxElement = document.getElementById("parish-combobox") as HTMLCalciteComboboxElement;
+
+  const { uniqueValueInfos } = await uniqueValues({
+    layer: countiesLayer,
+    field: "COUNTY"
+  });
+
+  uniqueValueInfos.forEach( uvi => {
+    const name = uvi.value as string;
+
+    const item = document.createElement("calcite-combobox-item");
+    item.value = name;
+    item.setAttribute("text-label", name);
+    countyComboBoxElement.appendChild(item);
+
+    const groupItem = document.createElement("calcite-combobox-item-group");
+    groupItem.label = name;
+    parishComboBoxElement.appendChild(groupItem);
+  });
 
   searchElement!.view = view as SearchWidget["view"];
   featuresElement!.view = view as FeaturesWidget["view"];
+  legendElement!.view = view as LegendWidget["view"];
 
   // Open the Features widget with features fetched from
   // the view click event location.
